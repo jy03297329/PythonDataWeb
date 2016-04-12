7fb3dcd3fd535a62400fc85684dbd59f04bde6a2
Yuanzheng Chang <hjcyz1991@gmail.com>
2016-04-11 23:36:55 -0400

    secomd commit

--- app.py
+++ app.py
@@ -4,14 +4,22 @@ Created on Thu Apr  7 13:15:12 2016
 
 @author: hjcyz1991
 """
-from flask import Flask
+from flask import Flask, render_template, jsonify
+from stock_scraper import get_data
+
+
 
 app = Flask(__name__)
 
 @app.route("/")
 def main():
-    return "Welcome!"
+    return render_template("index.html")
+
+@app.route("/data")
+def data():
+    return jsonify(get_data())
     
 if __name__ == "__main__":
     app.run()
-    
+
+
